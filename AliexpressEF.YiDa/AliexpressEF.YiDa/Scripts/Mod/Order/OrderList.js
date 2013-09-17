bcfe2e2cd792b6adee6642373fa4ee2c5e77821e
276398084 <276398084@qq.com>
2013-09-17 18:40:17 +0800

    grid&form  Test

--- AliexpressEF.YiDa/AliexpressEF.YiDa/Scripts/Mod/Order/OrderList.js
+++ AliexpressEF.YiDa/AliexpressEF.YiDa/Scripts/Mod/Order/OrderList.js
@@ -1,14 +1,5 @@
 ﻿
-Ext.define('ForumThread', {
-    extend: 'Ext.data.Model',
-    fields: [
-        'title', 'forumtitle', 'forumid', 'username',
-        { name: 'replycount', type: 'int' },
-        { name: 'lastpost', mapping: 'lastpost', type: 'date', dateFormat: 'timestamp' },
-        'lastposter', 'excerpt', 'threadid'
-    ],
-    idProperty: 'threadid'
-});
+
 // create the Data Store
 
 
@@ -31,14 +22,77 @@ var pluginExpanded = true;
 Ext.define('Mod.Order.OrderList', {
     extend: 'SXD.ex.ModuleInstance',
     alias: 'Mod.Order.OrderList',
+    getForm:function (){
+        var from = Ext.create('Ext.form.Panel', {
+            region: 'north',
+            fieldDefaults: {
+                labelWidth: 125,
+                autoFitErrors: false
+            },
+            defaultType: 'textfield',
+            items: [
+                {
+                    xtype: 'fieldcontainer',
+                    fieldLabel: 'Your Name',
+                    labelStyle: 'font-weight:bold;padding:0;',
+                    layout: 'hbox',
+                    defaultType: 'textfield',
+                    defaults: {
+                        labelWidth: 89,
+                        anchor: '100%',
+                        layout: {
+                            type: 'hbox',
+                            defaultMargins: { top: 0, right: 5, bottom: 0, left: 0 }
+                        }
+                    },
+                    items: [{
+                        width: 200,
+                        name: 'firstName',
+                        itemId: 'firstName',
+                        fieldLabel: 'First',
+                       
+                    }, {
+                        width: 200,
+                        name: 'middleInitial',
+                        fieldLabel: 'MI',
+                       
+                    }, {
+                        width: 200,
+                        name: 'lastName',
+                        fieldLabel: 'Last',
+                
+                       
+                    }]
+                }, {
+                fieldLabel: 'Start Date',
+                name: 'startdt',
+                itemId: 'startdt',
+              
+           
+            }, {
+                fieldLabel: 'End Date',
+                name: 'enddt',
+                itemId: 'enddt',
+               
+            }],
+            buttons: [{
+                text: 'Save',
+                handler: function () {
+                    this.up('form').getForm().isValid();
+                }
+            }, {
+                text: 'Cancel',
+                handler: function () {
+                    this.up('form').getForm().reset();
+                }
+            }]
+        });
+        return from;
+    },
     getGrid: function (store) {
         var grid = Ext.create('Ext.grid.Panel', {
-            width: 1200,
-            height: 650,
-            border: false,
-            forceFit: true,
-            frame: true,
-            layout: 'fit',
+           
+            region: 'center',
             store: store,
             disableSelection: true,
             loadMask: true,
@@ -84,7 +138,7 @@ Ext.define('Mod.Order.OrderList', {
             bbar: Ext.create('Ext.PagingToolbar', {
                 store: store,
                 displayInfo: true,
-                displayMsg: 'Displaying topics {0} - {1} of {2}',
+                displayMsg: '当前显示记录 {0} - {1} of {2}',
                 emptyMsg: "No topics to display",
                 items: [
                     '-', {
@@ -104,7 +158,7 @@ Ext.define('Mod.Order.OrderList', {
                 items: ['->', {
                     iconCls: 'icon-save',
                     itemId: 'save',
-                    text: 'Save',
+                    text: '同步订单',
                     disabled: true,
                     scope: this,
                     handler: this.onSave
@@ -149,10 +203,22 @@ Ext.define('Mod.Order.OrderList', {
     run: function () {
         var me = this;
         store = me.getStore();
+        me.north = me.getForm();
         me.center = me.getGrid(store);
         store.loadPage(1);
-        me.viewport.add([me.center]);
+        me.viewport.add([me.north,me.center]);
     }
+});
+
+Ext.define('ForumThread', {
+    extend: 'Ext.data.Model',
+    fields: [
+        'title', 'forumtitle', 'forumid', 'username',
+        { name: 'replycount', type: 'int' },
+        { name: 'lastpost', mapping: 'lastpost', type: 'date', dateFormat: 'timestamp' },
+        'lastposter', 'excerpt', 'threadid'
+    ],
+    idProperty: 'threadid'
 });
 
 
